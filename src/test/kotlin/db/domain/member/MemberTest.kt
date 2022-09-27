e87ff74b2ec922112739c33ea5ab8e3fd3814c2b
junuu <bababrll@naver.com>
2022-09-28 01:51:49 +0900

    test: member domain

---
+++ src/test/kotlin/db/domain/member/MemberTest.kt
@@ -0,0 +1,28 @@
+package db.domain.member
+
+import makeMember
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class MemberTest {
+    @Test
+    fun `회원은 id,userId,password,nickName,name을 가진다`(){
+        val member = makeMember()
+
+        Assertions.assertEquals(member.id, 1L)
+        Assertions.assertEquals(member.userId, "userId")
+        Assertions.assertEquals(member.password, "123456789")
+        Assertions.assertEquals(member.nickName, "junwoo")
+        Assertions.assertEquals(member.name, "김준우")
+    }
+
+    @Test
+    fun `회원은 nickName을 변경할 수 있다`(){
+        val member = makeMember()
+        val changedNickName = "changedNickName"
+
+        member.changeNickName(changedNickName)
+
+        Assertions.assertEquals(member.nickName, changedNickName)
+    }
+}
