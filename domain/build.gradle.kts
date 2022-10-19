963e8d23249246cb169a9cfda12e95f7ce556b11
junuu <bababrll@naver.com>
2022-10-19 21:40:03 +0900

    chore: bootjar false

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -23,3 +23,10 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-validation")
 }
 
+tasks.getByName("bootJar") {
+  enabled = false
+}
+tasks.getByName("jar") {
+  enabled = true
+}
+
