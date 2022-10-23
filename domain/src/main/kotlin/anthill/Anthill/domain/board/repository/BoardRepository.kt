7b56b56f59f1bb92240b3463d14295304d8fb2d2
junuu <bababrll@naver.com>
2022-10-23 20:57:08 +0900

    remove: unuse method remove, findByWriter()'

--- domain/src/main/kotlin/anthill/Anthill/domain/board/repository/BoardRepository.kt
+++ domain/src/main/kotlin/anthill/Anthill/domain/board/repository/BoardRepository.kt
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 
 interface BoardRepository : JpaRepository<Board, Long> {
-    fun findByWriter(writer: String): List<Board>
-
     @Modifying
     @Query("UPDATE Board b SET b.hits = b.hits + 1 WHERE b.id =:boardId")
     fun updateHitByBoardId(boardId: Long)
