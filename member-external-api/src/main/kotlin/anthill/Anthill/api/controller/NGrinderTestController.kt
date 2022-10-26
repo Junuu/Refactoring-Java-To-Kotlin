ef4ea9dd76f95b8f6846dfe11b5d1e22074bd353
junuu <bababrll@naver.com>
2022-10-27 01:25:21 +0900

    bug: @GetMapping is missing

--- member-external-api/src/main/kotlin/anthill/Anthill/api/controller/NGrinderTestController.kt
+++ member-external-api/src/main/kotlin/anthill/Anthill/api/controller/NGrinderTestController.kt
@@ -1,9 +1,14 @@
 package anthill.Anthill.api.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/stress-test")
 class NGrinderTestController {
+
+    @GetMapping
     fun helloNGrinder(): String{
         return "hello"
     }
